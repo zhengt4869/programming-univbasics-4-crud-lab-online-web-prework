6d7a936a68fb68d3ebfe4cd7fef984412093702e
Tianlin Zheng <zhengt@mit.edu>
2019-08-09 14:41:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,33 +7,33 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  return array.push(element)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  return array.unshift(element)
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  return array.
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  return array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  return array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array
 end
